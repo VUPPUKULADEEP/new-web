b0c316176b28ed5122efff683b3d156c3bae22b7
kuladeep <vuppukuladeep@gmail.com>
2025-01-23 19:45:41 +0530

    added some component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import About from '../components/about'
 
 function App() {
 
   return (
     <>
       <p>hello world</p>
+      <About/>
     </>
   )
 }
