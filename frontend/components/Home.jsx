c9f2d7371d2c14b66ed66a895c4ebe235c17f260
kuladeep <vuppukuladeep@gmail.com>
2025-02-02 16:10:21 +0530

    added api call

--- frontend/components/Home.jsx
+++ frontend/components/Home.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './About.css'
+import axios from 'axios';
 const Home = () => {
+  const call = async () => {
+    try {
+      const response = await axios.get( 'http://127.0.0.1:8000/project');
+      console.log('Data:', response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
   return (
     <div className='container'>
       <p>hello home</p>
+      <button onClick={call}>call</button>
     </div>
   )
 }
